f4b1988a5a5d2a83ba011d7ad6e54aaf7950d7da
ademosyodele <ademosyodele@users.noreply.github.com>
2026-03-11 14:39:13 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'memory-trainer-v1';
+const ASSETS = [
+    './',
+    'memory_trainer.html',
+    'https://cdn.jsdelivr.net/npm/chart.js',
+    'manifest.json',
+    'icon-192.png',
+    'icon-512.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+// Fetch from Cache
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => response || fetch(e.request))
+    );
+});
